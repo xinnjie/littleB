2685a1b637758b4e8077bd75b7bb9948f1cac704
xinnjie <xinnjie31@gmail.com>
2019-09-23 18:07:28 +0800

    littleb & example_executable 可编译

--- lib/role_info.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by xinnjie on 2019/9/13.
-//
-
-#include "role_info.h"
--- lib/role_info.h
+++ lib/role_info.h
@@ -4,7 +4,11 @@
 
 #ifndef LITTLEB_ROLE_INFO_H
 #define LITTLEB_ROLE_INFO_H
-
-class RoleInfo {};
+/**
+ * RoleInfo 作为纯数据存在
+ */
+struct RoleInfo {
+    int i;
+};
 
 #endif  // LITTLEB_ROLE_INFO_H
