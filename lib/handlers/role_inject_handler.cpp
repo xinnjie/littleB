b5a756c5c8557f32ccef0732db8e15b323db3112
xinnjie <xinnjie31@gmail.com>
2019-09-25 15:10:13 +0800

    role_inject_handler 实现

--- lib/handlers/role_inject_handler.cpp
+++ lib/handlers/role_inject_handler.cpp
@@ -1,5 +1,15 @@
 //
 // Created by xinnjie on 2019/9/25.
 //
-
 #include "role_inject_handler.h"
+#include <spdlog/spdlog.h>
+
+void littleB::RoleInjectHandler::read(Context *ctx, littleB::CmdMessagePair msg) {
+    auto addr = ctx->getTransport()->getPeerAddress();
+    std::shared_ptr<RoleInfo> role = role_manager_.GetRole(addr);
+    if (!role) {
+        SPDLOG_INFO("address={} not login or has some problem", addr.describe());
+        return;
+    }
+    ctx->fireRead(std::make_tuple(msg.first, role, std::move(msg.second)));
+}
--- lib/handlers/role_inject_handler.h
+++ lib/handlers/role_inject_handler.h
@@ -4,8 +4,18 @@
 
 #ifndef LITTLEB_ROLE_INJECT_HANDLER_H
 #define LITTLEB_ROLE_INJECT_HANDLER_H
+#include <wangle/channel/Handler.h>
+#include "common_def.h"
+#include "data_manager/roleinfo_manager.h"
 
 namespace littleB {
-class RoleInjectHandler {};
+class RoleInjectHandler : public wangle::InboundHandler<CmdMessagePair, ServiceTuple> {
+public:
+    explicit RoleInjectHandler(RoleinfoManager& roleManager) : role_manager_(roleManager) {}
+    void read(Context* ctx, CmdMessagePair msg) override;
+
+private:
+    RoleinfoManager &role_manager_;
+};
 }
 #endif  // LITTLEB_ROLE_INJECT_HANDLER_H
