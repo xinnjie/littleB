5e6b7920342fb3dcf2e6bbe81bee68be22f56ee9
xinnjie <xinnjie31@gmail.com>
2019-09-26 15:30:40 +0800

    feature: roleinfo_manager暂时加锁 todo: 针对连接 consistent hash到针对单线程的roleinfo_manager。这样线程间不会有冲突

--- lib/data_manager/roleinfo_manager.h
+++ lib/data_manager/roleinfo_manager.h
@@ -14,15 +14,18 @@ namespace littleB {
 class RoleinfoManager {
 public:
     bool AddRole(const folly::SocketAddress& remote_address, std::shared_ptr<RoleInfo> role_info) {
+        std::lock_guard<std::mutex> guard(roles_mutex_);
         return roles_.insert(std::make_pair(remote_address, role_info)).second;
     }
     bool RemoveRole(const folly::SocketAddress& remote_address) {
+        std::lock_guard<std::mutex> guard(roles_mutex_);
         return roles_.erase(remote_address) > 0;
     }
     std::shared_ptr<RoleInfo> GetRole(const folly::SocketAddress& remote_address);
 
 private:
     std::map<folly::SocketAddress, std::shared_ptr<RoleInfo>> roles_;
+    std::mutex roles_mutex_;
 };
 }  // namespace littleB
 #endif  // LITTLEB_ROLEINFO_MANAGER_H
