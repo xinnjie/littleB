d7e67059c7bf5855835734dcd1810aefa1e45b11
xinnjie <xinnjie31@gmail.com>
2019-10-04 02:49:06 +0800

    modify: 更新三方库

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -12,7 +12,7 @@ sudo apt-get install \
     zlib1g-dev \
     binutils-dev \
     libjemalloc-dev \
-    libssl-dev \
+    libssl-dev
 #    advance debuging functionality
 sudo apt-get install \
     libunwind8-dev \
